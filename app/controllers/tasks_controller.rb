20560dfcd1000245a03245bff26dc629f8b29c38
yanmer-lab <yanmer1998@ezweb.ne.jp>
2019-10-25 05:27:05 +0000

    CRUD task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,7 +13,7 @@ class TasksController < ApplicationController
   end
 
   def create
-　　　@task = Task.new(task_params)
+    @task = Task.new(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
